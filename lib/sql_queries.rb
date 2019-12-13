944ae1f205c977c824dbdbbacbc91d075b0224a5
Carla Holcomb <carla.programmer@gmail.com>
2019-12-13 02:18:50 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT name, age FROM bears WHERE gender = F;"
+  "SELECT name, age FROM bears WHERE gender = 'F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
@@ -7,7 +7,7 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age FROM bears WHERE alive = true ORDER BY age;"
+  "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
@@ -23,7 +23,7 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(id) FROM bears WHERE temperament = goofy;"
+  "SELECT COUNT(id) FROM bears WHERE temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
